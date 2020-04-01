aca1a6f970e713698906f923a846a9d670a866d6
Harsh Desai <hurshd0@gmail.com>
2020-04-01 21:43:29 +0000

    hursh: alex: notebooks: :alembic: experimenting with data cleaning of pop, census, and jhu data

--- scripts/state_county_model_mongo.py
+++ scripts/state_county_model_mongo.py
@@ -1,7 +1,8 @@
 from mongoengine import *
 
+
 class Stats(EmbeddedDocument):
-    last_updated = DateTimeField(required=False)
+    last_updated = DateTimeField(required=True)
     tested = IntField(required=False)
     confirmed = IntField(required=True)
     deaths = IntField(required=True)
@@ -13,8 +14,8 @@ class Country(Document):
     lat = FloatField(required=True)
     lon = FloatField(required=True)
     population = IntField(required=False)
-    area = IntField(required=False)
-    stats = ListField(EmbeddedDocumentField(Stats, required=False))
+    area = FloatField(required=False)
+    stats = ListField(EmbeddedDocumentField(Stats, required=True))
 
 
 class State(Document):
@@ -23,61 +24,63 @@ class State(Document):
     lat = FloatField(required=True)
     lon = FloatField(required=True)
     population = IntField(required=False)
-    area = IntField(required=False)
-    stats = ListField(EmbeddedDocumentField(Stats, required=False))
+    area = FloatField(required=False)
+    stats = ListField(EmbeddedDocumentField(Stats, required=True))
 
 
 class County(Document):
-    state = StringField(max_length=50, required=True)
+    state = StringField(max_length=15, required=True)
     stateAbbr = StringField(max_length=2, required=True)
     county = StringField(max_length=100, require=True)
     fips = IntField(max_length=6, required=True)
     lat = FloatField(required=True)
     lon = FloatField(required=True)
-    population = IntField(required=False)
-    area = IntField(required=False)
+    population = IntField(required=True)
+    area = FloatField(required=True)
     hospitals = IntField(required=False)
     hospital_beds = IntField(required=False)
-    medium_income = IntField(required=False)
-    stats = ListField(EmbeddedDocumentField(Stats, required=False))
+    medium_income = FloatField(required=True)
+    stats = ListField(EmbeddedDocumentField(Stats, required=True))
+
 
 
 
 def main():
-    connect(host=config("MONGODB_CONNECTION_URI"))
+    # connect(host=config("MONGODB_CONNECTION_STAGING_URI"))
 
-    county = County(state="Michigan",
-                    stateAbbr="MI",
-                    county="Deer Field",
-                    fips=12345,
-                    lat=12.34567,
-                    lon=89.01234,
-                    stats=Stats(
-                        last_updated="2020-03-31",
-                        confirmed=12345,
-                        deaths=67890,
-                        )
-                    )
-    county.save()
+    # county = County(state="Michigan",
+    #                 stateAbbr="MI",
+    #                 county="Deer Field",
+    #                 fips=12345,
+    #                 lat=12.34567,
+    #                 lon=89.01234,
+    #                 stats=Stats(
+    #                     last_updated="2020-03-31",
+    #                     confirmed=12345,
+    #                     deaths=67890,
+    #                     )
+    #                 )
+    # county.save()
 
-    country = Country(country="South Korea",
-                alpha2Code="SK",
-                lat=12.34567,
-                lon=89.01234,
-                stats=Stats(
-                    last_updated="2020-03-31",
-                    confirmed=12345,
-                    deaths=67890,
-                    )
-                )
-    country.save()
+    # country = Country(country="South Korea",
+    #             alpha2Code="SK",
+    #             lat=12.34567,
+    #             lon=89.01234,
+    #             stats=Stats(
+    #                 last_updated="2020-03-31",
+    #                 confirmed=12345,
+    #                 deaths=67890,
+    #                 )
+    #             )
+    # country.save()
 
 
-    for county in County.objects:
-        print(county.state)
+    # for county in County.objects:
+    #     print(county.state)
 
-    disconnect()
+    # disconnect()
 
+    pass
 
 if __name__ == "__main__":
     main()
